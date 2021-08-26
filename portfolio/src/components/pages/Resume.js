58a6669631af1834d894e4c5a88df5978b0fa898
Charles Glass <charlie-glass@live.com>
2021-08-25 22:29:23 -0700

    portfolio

---
+++ portfolio/src/components/pages/Resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>download my <a href="https://drive.google.com/file/d/1owUAKC6lsVqcJuxdQafCQS1o-Y72udqZ/view?usp=sharing" target="_blank" rel="noreferrer">resume</a></p>
+      <h5>Languages:</h5> 
+      <p>Python, JavaScript, SQL, HTML, CSS</p>
+      <h5>Tools:</h5> 
+      <p>Git, ReactJS, Rest APIS, EJS, JupyterLab, Chrome DEV Tools</p>
+    </div>
+  );
+}
